a45706b72d661a84ed1457a02a332e3d58860415
haein <haein@haeinui-MacBookPro.local>
2023-08-31 19:12:06 +0900

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+//100vh 브라우저 환경 커버하기
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// resize
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
